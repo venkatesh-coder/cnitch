9b64f64dec37562da66ce8224cca88b13f43651c
venkatesh <shariharanvenkatesh@gmail.com>
2022-01-22 16:16:05 +0000

    Added Hashtable_unload()

--- src/main.c
+++ src/main.c
@@ -14,25 +14,6 @@
 #include "util.h"
 
 
-int main1(void)
-{
-    char *file_path = "Makefile";
-    FNode *fnode = HashTable_create_node(file_path);
-    assert(fnode != NULL);
-    HashTable_add_file_path(fnode);
-    char *fp1 = "src/list.h";
-    FNode *fnode1 = HashTable_create_node(fp1);
-    HashTable_add_file_path(fnode1);
-    FNode *f = HashTable_find_file(file_path);
-    if (f != NULL)
-        printf("found: %s\n", f->file_path);
-    else
-        printf("not found: %s\n", file_path);
-    HashTable_print();
-    HashTable_unload();
-    return 0;
-}
-
 int main(void)
 {
     char *path = strdup(".");
@@ -45,23 +26,19 @@ int main(void)
         RECDIR_Frame *top = recdir_top(recdirp);
         char *dir_path = top->path;
         uint32_t base_len = strlen(dir_path);
-        char file_path[base_len + MAX_FILENAME_LEN];
+        char file_path[base_len + MAX_FILENAME_LEN + 2];
         strcpy(file_path, dir_path);
         char *file_path_p = join_path(file_path, ent->d_name); 
 
-        printf("file: %s\n", file_path_p);
         Com_mode_find_and_add_todos(&com_ent_list, file_path_p);
         ent = recdir_read(recdirp);
     }
     recdir_close(recdirp);
     Com_mode_list_print_entries(com_ent_list);
+
+    HashTable_unload();     // this uses global structure.
     Com_mode_list_free_entries(com_ent_list);
 
     free(recdirp);
     return 0;
 }
-
-/* int main(void) */
-/* { */
-/*     return 0; */
-/* } */
